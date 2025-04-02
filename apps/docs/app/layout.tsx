d10476a9b89e9e7d5e2601d67b040864fa6af796
Akash Gupta <ag308669@gmail.com>
2025-04-02 23:51:33 +0530

    slight refactor

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,30 +1,37 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist, Geist_Mono } from "next/font/google";
 
-import "@workspace/ui/globals.css"
-import { Providers } from "@/components/providers"
+import "@workspace/ui/globals.css";
+import { Providers } from "@/components/providers";
+import { cn } from "@workspace/ui/lib/utils";
 
 const fontSans = Geist({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
-})
+});
 
-export default function RootLayout({
+const RootLayout = ({
   children,
 }: Readonly<{
-  children: React.ReactNode
-}>) {
+  children: React.ReactNode;
+}>) => {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
+        className={cn(
+          "font-sans antialiased",
+          fontSans.variable,
+          fontMono.variable
+        )}
       >
         <Providers>{children}</Providers>
       </body>
     </html>
-  )
-}
+  );
+};
+
+export default RootLayout;
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@workspace/ui/components/button";
 
-export default function Page() {
+const HomePage = () => {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">This is docs</h1>
-        <Button size="sm">Button</Button>
+        <h1 className="text-2xl font-bold">This is web app</h1>
+        <Button>Button</Button>
       </div>
     </div>
   );
-}
+};
+
+export default HomePage;
