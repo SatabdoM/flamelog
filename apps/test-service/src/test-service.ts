6780d1d279a9043b4b6aa334b2ce2e19361792b2
SatabdoM <satabdomajumdertech@gmail.com>
2025-04-19 19:26:35 +0530

    Added test-service config inside Turbo.json

---
+++ apps/test-service/src/test-service.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables
+
+const app = express();
+const PORT = 5001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Basic route
+app.get('/', (_, res) => {
+  res.send('Flamelog Server is Running! ');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
