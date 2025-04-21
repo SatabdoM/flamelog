2c2ff0ac7e306f1a6d617e953b01eaee965ff82f
SatabdoM <satabdomajumdertech@gmail.com>
2025-04-21 22:11:36 +0530

    Added routes and app router

--- apps/test-service/src/test-service.ts
+++ apps/test-service/src/test-service.ts
@@ -1,24 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import router from './routes';
 
-import { PrismaClient } from '@prisma/client';
-
-dotenv.config(); // Load environment variables
+dotenv.config();
 
 const app = express();
 const PORT = 5001;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
+app.use('/api', router);
 
 // Basic route
 app.get('/', (_, res) => {
-  res.send('Flamelog Server is Running! ');
+  res.send('Working apis start with /api');
 });
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Flamelog Test-Service running on http://localhost:${PORT}`);
 });
