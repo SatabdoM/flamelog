89667aa8ea44b7f73999fde82283cc7d0fcdf25a
Akash Gupta <ag308669@gmail.com>
2025-04-27 22:03:33 +0530

    Integrate rich text editor (tiptap)

--- apps/web/app/(main)/feed/page.tsx
+++ apps/web/app/(main)/feed/page.tsx
@@ -1,10 +1,4 @@
-'use client';
-
-import { useAuthStore } from '@/store/auth';
-
 const FeedPage = () => {
-  const authState = useAuthStore();
-  console.log(authState);
   return <div>FeedPage</div>;
 };
 
--- apps/web/app/(main)/post/page.tsx
+++ apps/web/app/(main)/post/page.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+'use client';
+
+import { SimpleEditor } from '@/components/editor';
 
 const PostPage = () => {
-  return <div>PostPage</div>;
+  return (
+    <div className="space-y-6">
+      <h1>Post Page</h1>
+      <div className="bg-accent h-[800px] rounded-md p-2">
+        <SimpleEditor />
+      </div>
+
+      <div>
+        <p>Hello there</p>
+      </div>
+    </div>
+  );
 };
 
 export default PostPage;
